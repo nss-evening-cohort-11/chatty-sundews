0b5bcbd2553aa86376e84cb509658d346ea8826a
Sarah Holder <sr.holder@yahoo.com>
2020-03-12 17:42:18 -0500

    wip

--- src/javascripts/components/navbar/navbar.js
+++ src/javascripts/components/navbar/navbar.js
@@ -2,27 +2,35 @@ import utilities from '../../helpers/utilities';
 import getInfo from '../../helpers/data/userData';
 import './navbar.scss';
 
-const loadNavbar = () => {
+const radioButtons = () => {
   const radioUser = getInfo.getUsers();
   let domString = '';
-  domString += '<nav class="nav fixed-top navbar-light bg-light container-fluid>';
-  domString += '  <div class="img-container">';
-  domString += '    <img class="logo" src="src/img/sundew-smoozer.png" alt="Sundew Smoozers"></div>';
-  domString += '      <div class="user-display col d-flex justify-content-center"></div>';
   radioUser.forEach((user) => {
-    domString += '      <div class="form-check-inline">';
-    domString += '        <label>';
-    domString += '            <input class="form-check-input" type="radio" name="inlineRadioOptions" value="small">';
-    domString += `            <img class="p-1 form-check-label rounded-circle" src="${user.imgUrl}" width="60px" height="60px">`;
-    domString += `            <p class="text-center">${user.name}</p>`;
-    domString += '        </label>';
-    domString += '      </div>';
+    domString += '<div class="form-check-inline">';
+    domString += '  <label>';
+    domString += '    <input class="form-check-input" type="radio" name="inlineRadioOptions" value="small">';
+    domString += `    <img class="p-1 form-check-label rounded-circle" src="${user.imgUrl}" width="60px" height="60px">`;
+    domString += `    <p class="text-center">${user.name}</p>`;
+    domString += '   </label>';
+    domString += '</div>';
   });
-  domString += '      </div>';
-  domString += '  </div>';
-  domString += '  <div class="col-4 flex align-self-center">';
+  return domString;
+};
+const loadNavbar = () => {
+  let domString = '';
+  domString += '<nav class="navbar navbar-expand-lg fixed-top navbar-light bg-light container-fluid>';
+  domString += '    <div class="navbar-header>';
+  domString += '      <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigationBar" aria-expanded="false" aria-label="Toggle navigation">';
+  domString += '      <span class="navbar-toggler-icon"></span>';
+  domString += '      </button>';
+  domString += '        <img class="logo" src="src/img/sundew-smoozer.png" alt="Sundew Smoozers">';
+  domString += '    <div class="collapse navbar-collapse">';
+  domString += '        <div class="user-display d-flex justify-content-center">';
+  domString += `            <div>${radioButtons()}`;
+  domString += '        </div>';
+  domString += '      <div class="col-2 flex align-self-center">';
   domString += '    <div class="form-check row">';
-  domString += '      <input class="form-check-input" type="checkbox" value="dark theme" id="defaultCheck1">';
+  domString += '      <input class="form-check-input" type="checkbox" value="dark-theme" id="defaultCheck1">';
   domString += '      <label class="form-check-label" for="defaultCheck1">Dark Theme</label>';
   domString += '    </div>';
   domString += '    <div class="form-check row">';
@@ -31,10 +39,11 @@ const loadNavbar = () => {
   domString += '    </div>';
   domString += '  </div>';
   domString += '<div class="container-fluid pb-3">';
-  domString += '    <form class="col-9 offset-md-2 d-inline-flex">';
-  domString += '      <input class="form-control col-8" type="text" placeholder="Enter message and hit ENTER" aria-label="text">';
+  domString += '    <form class="col offset-md-2 d-inline-flex">';
+  domString += '      <input class="form-control col-10" type="text" placeholder="Select User above, add message and hit ENTER" aria-label="text">';
   domString += '      <button class="btn btn-secondary btn-block ml-2">CLEAR</button>';
   domString += '    </form>';
+  domString += '</div>';
   domString += '</div>';
   domString += '</nav>';
 
