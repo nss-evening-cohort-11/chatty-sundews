64cf7a286897b65a4412fbbff83ead9c7652fd29
Sarah Holder <sr.holder@yahoo.com>
2020-03-11 20:28:25 -0500

    fixing errors

---
+++ src/javascripts/components/navbar/navbar.js
@@ -0,0 +1,22 @@
+import utilities from '../../helpers/utilities';
+import './navbar.scss';
+
+const loadNavbar = () => {
+  let domString = '';
+  domString += '<nav>';
+  domString += '<div class="nav fixed-top navbar-light bg-light">';
+  domString += '  <img src="src/img/sundew-smoozer.png"  alt="Sundew Smoozers Logo" height="100">';
+  domString += '   <form>';
+  domString += '     <input class="form-control" type="text" placeholder="Enter message and hit ENTER" aria-label="text">';
+  domString += '     <button class="btn btn-outline-success">Clear</>';
+  domString += '   </form></div>';
+  domString += '';
+  domString += '';
+  domString += '';
+  domString += '';
+  domString += '';
+
+  utilities.printToDom('nav-container', domString);
+};
+
+export default { loadNavbar };
