c5de08cd67149c5cb581ba6d92fdcbb2cf6a7364
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-03-12 14:30:55 -0500

    added styling to displayMessages

--- src/javascripts/components/displayMessage/displayMessage.js
+++ src/javascripts/components/displayMessage/displayMessage.js
@@ -7,7 +7,12 @@ const displayAllMessages = () => {
   const chatMessages = messageData.getMessages();
   chatMessages.forEach((message) => {
     domString += `
-      <div class="card text-center">${message.name}</div>
+    <div class="messageCard" style="margin-right: 5px; margin-left: auto;">
+    <span class="closebtn" id="${message.id}">×</span>
+    <h3 class="messageName">${message.name}</h3>
+    <p class="messageText text-center">${message.text}</p>
+    <div class="timestamp">${message.timestamp}</div>
+  </div>
     `;
   });
   utilities.printToDom('message-container', domString);
