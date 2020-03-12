506d87fcf615beb39db03d6b31c172a479648654
Jose Marquez <josesinmarquezpalmeros@gmail.com>
2020-03-11 19:55:01 -0500

    got the foundations for the messageLimit function

--- src/javascripts/components/messageLimit/messageLimit.js
+++ src/javascripts/components/messageLimit/messageLimit.js
@@ -3,7 +3,8 @@ import messages from '../../helpers/data/messageData';
 
 const getMessageLimit = () => {
   const myMessages = messages.getMessages();
-  console.error(myMessages);
+  const newMessages = myMessages.slice(-20);
+  console.error('myMessages', myMessages, 'newMessages', newMessages);
 };
 
 export default { getMessageLimit };
