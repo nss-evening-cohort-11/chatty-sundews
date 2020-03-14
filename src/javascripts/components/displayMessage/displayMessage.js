64f7d952d73f1400691eb5d67dc00175f4052b33
Anca Simon <ancarsimon@gmail.com>
2020-03-14 14:43:33 -0500

    disabled image code for new messages

--- src/javascripts/components/displayMessage/displayMessage.js
+++ src/javascripts/components/displayMessage/displayMessage.js
@@ -1,20 +1,20 @@
 import './displayMessage.scss';
 import messageData from '../../helpers/data/messageData';
-import userData from '../../helpers/data/userData';
+// import userData from '../../helpers/data/userData';
 import utilities from '../../helpers/utilities';
 
 const displayAllMessages = () => {
-  const users = userData.getUsers();
+  // const users = userData.getUsers();
   const allMessages = messageData.getMessages();
   let domString = '';
   allMessages.forEach((message) => {
-    const user = users.find((x) => x.id === message.id);
+    // const user = users.find((x) => x.id === message.id);
     domString += `
     <div id="${message.messageId}" class="messageCard card mb-3">
       <div id="close-button" class="closebtn"><i class="fas fa-trash"></i>
       </div>
       <div>
-      <img src=${user.imgUrl} class="img-rounded col-3" alt="user">
+
           <div class="card-body p-0">
               <p class="messageText text-center card-text">${message.text}</p>
               <p class="timestamp"><small>${message.timestamp}</small></p>
@@ -24,6 +24,9 @@ const displayAllMessages = () => {
   });
   utilities.printToDom('message-container', domString);
 };
+
+// NOTE: deleted this from above card-body div in domString above: DELETE this note when user info added to a new message.
+//       <img src=${user.imgUrl} class="img-rounded col-3" alt="user">
 
 const deleteMessageEvent = (e) => {
   const selectedMessage = e.target.closest('.card').id;
