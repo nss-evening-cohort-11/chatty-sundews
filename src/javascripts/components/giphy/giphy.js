7359ffac34ca3c45c64dc60d86ff47534f5e7c8a
Jose Marquez <josesinmarquezpalmeros@gmail.com>
2020-03-14 11:00:29 -0500

    wip

--- src/javascripts/components/giphy/giphy.js
+++ src/javascripts/components/giphy/giphy.js
@@ -1,8 +1,11 @@
+const testGiphy = (data) => {
+  console.error(data);
+  console.error(data.data[1].images.original.url);
+};
 
 const giphy = () => {
   const xhr = $.get('http://api.giphy.com/v1/gifs/search?q=rainbow&api_key=KWlbcPJCe7ChYy6da2xuocbKh0K2XGwO&limit=5');
-  xhr.done((data) => { console.error('success got data', data); });
-  return xhr;
+  xhr.done((data) => testGiphy(data));
 };
 
 const giphyFunction = () => {
